17629940f6332829be41ab6c75401802eb655cc1
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-10-26 10:43:55 +0200

    updating code and tests

--- test.js
+++ test.js
@@ -1,31 +1,41 @@
 var utilityIVA = require('./utility-iva');
 
-console.log('scorporoIVA(1220, 22) = ' + utilityIVA.scorporoIVA(1220, 22));
-utilityIVA.scorporoIVA(1220, 22, function(err, data){
-    console.log(data);
+it('scorporoIVA should work as expected', function (done) {
+  expect(utilityIVA.scorporoIVA(1220, 22)).toBe(1000);
+  utilityIVA.scorporoIVA(1220, 22, function (err, data) {
+    expect(data).toEqual({ imponibile: 1000, aliquotaIVA: 22, importoIVA: 220, totale: 1220 });
+  });
+  done();
 });
-console.log('\n');
 
-console.log('calcolaImportoIvato(1000, 21) = ' + utilityIVA.calcolaImportoIvato(1000, 21));
-utilityIVA.calcolaImportoIvato(1000, 21, function(err, data){
-    console.log(data);
+it('calcolaImportoIvato should work as expected', function (done) {
+  expect(utilityIVA.calcolaImportoIvato(1000, 21)).toBe(1210);
+  utilityIVA.calcolaImportoIvato(1000, 21, function (err, data) {
+    expect(data).toEqual({ imponibile: 1000, aliquotaIVA: 21, importoIVA: 210, totale: 1210 });
+  });
+  done();
 });
-console.log('\n');
 
-console.log('calcolaAliquotaIVA(1000, 1230) = ' + utilityIVA.calcolaAliquotaIVA(1000, 1230));
-utilityIVA.calcolaAliquotaIVA(1000, 1230, function(err, data){
-    console.log(data);
+it('calcolaAliquotaIVA should work as expected', function (done) {
+  expect(utilityIVA.calcolaAliquotaIVA(1000, 1230)).toBe(23);
+  utilityIVA.calcolaAliquotaIVA(1000, 1230, function (err, data) {
+    expect(data).toEqual({ aliquotaIVA: 23, imponibile: 1000, importoIVA: 230, totale: 1230 });
+  });
+  done();
 });
-console.log('\n');
 
-console.log('calcolaImponibileDaIVA(200, 20) = ' + utilityIVA.calcolaImponibileDaIVA(200, 20));
-utilityIVA.calcolaImponibileDaIVA(200, 20, function(err, data){
-    console.log(data);
+it('calcolaImponibileDaIVA should work as expected', function (done) {
+  expect(utilityIVA.calcolaImponibileDaIVA(200, 20)).toBe(1000);
+  utilityIVA.calcolaImponibileDaIVA(200, 20, function (err, data) {
+    expect(data).toEqual({ imponibile: 1000, aliquotaIVA: 20, importoIVA: 200, totale: 1200 });
+  });
+  done();
 });
-console.log('\n');
 
-console.log('calcolaTotaleDaIVA(200, 20) = ' + utilityIVA.calcolaTotaleDaIVA(200, 20));
-utilityIVA.calcolaTotaleDaIVA(200, 20, function(err, data){
-    console.log(data);
+it('calcolaTotaleDaIVA should work as expected', function (done) {
+  expect(utilityIVA.calcolaTotaleDaIVA(200, 20)).toBe(1200);
+  utilityIVA.calcolaTotaleDaIVA(200, 20, function (err, data) {
+    expect(data).toEqual({ imponibile: 1000, aliquotaIVA: 20, importoIVA: 200, totale: 1200 });
+  });
+  done();
 });
-console.log('\n');
